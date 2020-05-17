35dba969af1769550ec62734649941e2d4b32570
Pranita Patel <ppatel23@in.ibm.com>
2020-05-17 20:58:36 +0530

    Updates

---
+++ precache-manifest.f46abdf87de26f5ea787e0d3bdaf94d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2800a99f7950bfb0984e43660a802ee9",
+    "url": "/I-Am-Pranita-Patel/index.html"
+  },
+  {
+    "revision": "706c1853a744afb90a07",
+    "url": "/I-Am-Pranita-Patel/static/js/2.d23efdbc.chunk.js"
+  },
+  {
+    "revision": "67c9ed6a8a94a5a5d3969134c2e73fd9",
+    "url": "/I-Am-Pranita-Patel/static/js/2.d23efdbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "542f8ac1b7425ee6dbd6",
+    "url": "/I-Am-Pranita-Patel/static/js/main.ac6d9d47.chunk.js"
+  },
+  {
+    "revision": "34c9cc991f83d5087dbc",
+    "url": "/I-Am-Pranita-Patel/static/js/runtime-main.207261be.js"
+  }
+]);
